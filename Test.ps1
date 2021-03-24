25e5797f76466b40b4831d19554e65ae130d2026
Ibbz <ibzz90@github.com>
2021-03-24 21:42:49 +0000

    removed test file

--- Test.ps1
+++
@@ -1,8 +0,0 @@
-
-$AsyncJob = [PowerShell]::Create()
-$null = $AsyncJob.AddScript({
-    Param($Object)
-    & $Object.File -Config $Object.Config -Message $Object.Message)
-}).AddArgument(@{File =  ".\Recipes\Play-Beep\Main.ps1"; Config = $Config; Topic = $Topic ; $Message =  ([System.Text.Encoding]::ASCII.GetString($MqttObject.Message})
-$AsyncInvoke = $AsyncJob.BeginInvoke()
-
